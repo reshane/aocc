36001a93ec01d67a7e4e4df52633c26312924ace
reshane <shanekenny023@gmail.com>
2026-01-03 16:07:11 -0500

    enable all warnings

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ if [ ! -d "./build" ]; then
     mkdir -p ./build
 fi
 
-gcc -c ./src/lib/aoc.c -o ./build/aoc.o
+gcc -c ./src/lib/aoc.c -o ./build/aoc.o -Wall -Wextra
 ar rcs ./build/libaoc.a ./build/aoc.o
 
-gcc ./src/*.c -DTESTING -L./build -laoc -o ./build/test
-gcc ./src/*.c -L./build -laoc -O3 -o ./build/main
+gcc ./src/*.c -Wall -Wextra -DTESTING -L./build -laoc -o ./build/test
+gcc ./src/*.c -Wall -Wextra -L./build -laoc -O3 -o ./build/main
