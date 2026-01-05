1999205fff7c8d14258272501775656cff8b3f20
reshane <shanekenny023@gmail.com>
2026-01-04 19:28:19 -0500

    Add input fetching script

---
+++ get_inputs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "$COOKIE" ]; then
+    echo "Error: COOKIE is not set" 1>&2
+    exit 1
+fi
+
+for i in {1..10}
+do
+    printf -v padded "%02d" "$i"
+
+    curl -b "session=$COOKIE" \
+        "https://adventofcode.com/2025/day/$i/input" > inputs/$padded.txt
+done
