ba9dabea51e50d30bcc91f79036f04c7ca9f6de2
reshane <shanekenny023@gmail.com>
2026-01-09 17:39:13 -0500

    format

--- src/day11.c
+++ src/day11.c
@@ -1,7 +1,4 @@
-
 #include "lib/aoc.h"
-#include <stdint.h>
-#include <sys/cdefs.h>
 
 #define D11_MAX_LNS 1024
 
@@ -11,12 +8,13 @@ uint32_t hash(StringView *node_str) {
     //     h <<= 8;
     //     h |= node_str->buf[i];
     // }
-    // uint32_t h = ((uint32_t)0xFFFFFF00 & *(uint32_t*)((void*)node_str->buf)) >> 8;
-    // uint32_t h = (*(uint32_t*)(node_str->buf)) >> 8;
-    // printf("[%p %p %p] -> %"PRIu32"\n",
-    // uint32_t h = ((((uint32_t)(uint8_t)node_str->buf[0]) << 16
+    // uint32_t h = ((uint32_t)0xFFFFFF00 & *(uint32_t*)((void*)node_str->buf))
+    // >> 8; uint32_t h = (*(uint32_t*)(node_str->buf)) >> 8; printf("[%p %p %p]
+    // -> %"PRIu32"\n", uint32_t h = ((((uint32_t)(uint8_t)node_str->buf[0]) <<
+    // 16
     //                | ((uint32_t)(uint8_t)node_str->buf[1]) << 8
-    //                | ((uint32_t)(uint8_t)node_str->buf[2])) - 0x616161) % D11_MAX_LNS;
+    //                | ((uint32_t)(uint8_t)node_str->buf[2])) - 0x616161) %
+    //                D11_MAX_LNS;
     // printf("["); sv_print(node_str); printf("] %"PRIu32"\n", h);
 
     uint32_t h = 5381;
@@ -31,9 +29,11 @@ uint32_t hash(StringView *node_str) {
 uint32_t get_index(StringView *node_str, StringView *nodes, int *populated) {
     uint32_t node_hash = hash(node_str);
     if (populated[node_hash]) {
-        while (sv_cmp(node_str, &nodes[node_hash]) != 0 && node_hash < D11_MAX_LNS) {
+        while (sv_cmp(node_str, &nodes[node_hash]) != 0 &&
+               node_hash < D11_MAX_LNS) {
             // printf("Nodes collide! ["); sv_print(&node_str); printf("] & [");
-            // sv_print(&node_strs[node_hash]); printf("] -> %"PRIu32"\n", node_hash);
+            // sv_print(&node_strs[node_hash]); printf("] -> %"PRIu32"\n",
+            // node_hash);
             node_hash++;
         }
     }
@@ -57,9 +57,11 @@ typedef struct {
 uint32_t d11_set_insert(d11set *set, StringView *node_str) {
     uint32_t node_hash = hash(node_str);
     if (set->populated[node_hash]) {
-        while (sv_cmp(node_str, &set->nodes[node_hash]) != 0 && node_hash < D11_MAX_LNS) {
+        while (sv_cmp(node_str, &set->nodes[node_hash]) != 0 &&
+               node_hash < D11_MAX_LNS) {
             // printf("Nodes collide! ["); sv_print(&node_str); printf("] & [");
-            // sv_print(&node_strs[node_hash]); printf("] -> %"PRIu32"\n", node_hash);
+            // sv_print(&node_strs[node_hash]); printf("] -> %"PRIu32"\n",
+            // node_hash);
             node_hash++;
         }
     }
@@ -86,10 +88,10 @@ typedef struct {
 
 #define D11_Q_CAP 4096
 
-void qpush11(d11queue *q, d11qf e)
-{
+void qpush11(d11queue *q, d11qf e) {
     if (q->size == D11_Q_CAP) {
-        fprintf(stderr, "Could not push to queue, too many elements %zu > %d\n", q->size, D11_Q_CAP);
+        fprintf(stderr, "Could not push to queue, too many elements %zu > %d\n",
+                q->size, D11_Q_CAP);
         exit(1);
     }
     q->data[q->qb++] = e;
@@ -97,8 +99,7 @@ void qpush11(d11queue *q, d11qf e)
     q->size += 1;
 }
 
-d11qf qpop11(d11queue *q)
-{
+d11qf qpop11(d11queue *q) {
     if (q->size == 0) {
         fprintf(stderr, "Could not pop from an empty queue\n");
         exit(1);
@@ -110,18 +111,20 @@ d11qf qpop11(d11queue *q)
     return e;
 }
 
-long long d11_dist(uint32_t start, uint32_t end, uint32_t nbors[D11_MAX_LNS][D11_MAX_LNS], size_t *nbor_cts) {
+long long d11_dist(uint32_t start, uint32_t end,
+                   uint32_t nbors[D11_MAX_LNS][D11_MAX_LNS], size_t *nbor_cts) {
     d11qf *qdat = (d11qf *)malloc(sizeof(d11qf) * D11_Q_CAP);
     d11queue queue = {.data = qdat, .qh = 0, .qb = 0, .size = 0};
 
-    qpush11(&queue, (d11qf){ .data = start });
+    qpush11(&queue, (d11qf){.data = start});
 
     // bfs
     // every time we encounter end, increment
     long long total = 0;
     while (queue.size != 0) {
         d11qf curr = qpop11(&queue);
-        // printf("curr: %"PRIu32" -> ", curr.data); sv_print(&node_strs[curr.data]); printf("\n");
+        // printf("curr: %"PRIu32" -> ", curr.data);
+        // sv_print(&node_strs[curr.data]); printf("\n");
         uint32_t *curr_nbors = nbors[curr.data];
         size_t curr_nbors_ct = nbor_cts[curr.data];
         for (size_t i = 0; i < curr_nbors_ct; ++i) {
@@ -129,7 +132,7 @@ long long d11_dist(uint32_t start, uint32_t end, uint32_t nbors[D11_MAX_LNS][D11
             if (nbor == end) {
                 total++;
             } else {
-                qpush11(&queue, (d11qf) { .data = nbor });
+                qpush11(&queue, (d11qf){.data = nbor});
             }
         }
     }
@@ -139,8 +142,7 @@ long long d11_dist(uint32_t start, uint32_t end, uint32_t nbors[D11_MAX_LNS][D11
     return total;
 }
 
-long long d11_solve_p1(char *input, const size_t input_sz)
-{
+long long d11_solve_p1(char *input, const size_t input_sz) {
     StringView lns = {0};
     size_t lns_ct = 0;
     Splitter split = {.buf = input, .mx = input_sz};
@@ -154,21 +156,24 @@ long long d11_solve_p1(char *input, const size_t input_sz)
     while (lines(&split)) {
         assert("lns ran out of space!" && lns_ct < D11_MAX_LNS);
         split_curr(&split, &lns);
-        Splitter ln_split = {.buf=lns.buf, .mx=lns.len};
+        Splitter ln_split = {.buf = lns.buf, .mx = lns.len};
         delim(&ln_split, ' ');
         StringView node_str;
         split_curr(&ln_split, &node_str);
         --node_str.len;
 
         uint32_t node_idx = get_index(&node_str, &node_strs[0], &populated[0]);
-        // printf("["); sv_print(&node_str); printf("](%"PRIu32")--> ", node_hash);
+        // printf("["); sv_print(&node_str); printf("](%"PRIu32")--> ",
+        // node_hash);
         while (delim(&ln_split, ' ')) {
             StringView nbor_str;
             split_curr(&ln_split, &nbor_str);
-            if (nbor_str.buf[nbor_str.len-1] == '\n') --nbor_str.len;
+            if (nbor_str.buf[nbor_str.len - 1] == '\n') --nbor_str.len;
 
-            uint32_t nbor_idx = get_index(&nbor_str, &node_strs[0], &populated[0]);
-            // printf("["); sv_print(&nbor_str); printf("](%"PRIu32") ", nbor_hash);
+            uint32_t nbor_idx =
+                get_index(&nbor_str, &node_strs[0], &populated[0]);
+            // printf("["); sv_print(&nbor_str); printf("](%"PRIu32") ",
+            // nbor_hash);
             nbors[node_idx][nbor_cts[node_idx]] = nbor_idx;
             nbor_cts[node_idx] += 1;
         }
@@ -178,19 +183,22 @@ long long d11_solve_p1(char *input, const size_t input_sz)
 
     // int visited[D11_MAX_LNS] = {0};
     StringView start_str = {.buf = "you", .len = 3};
-    StringView end_str   = {.buf = "out", .len = 3};
+    StringView end_str = {.buf = "out", .len = 3};
     uint32_t start = get_index(&start_str, &node_strs[0], &populated[0]);
-    uint32_t end   = get_index(&end_str,   &node_strs[0], &populated[0]);
+    uint32_t end = get_index(&end_str, &node_strs[0], &populated[0]);
     return d11_dist(start, end, nbors, nbor_cts);
 }
 
-long long paths(uint32_t src, uint32_t dst, uint32_t *po, size_t pol, uint32_t nbors[D11_MAX_LNS][D11_MAX_LNS], size_t nbor_cts[D11_MAX_LNS]) {
+long long paths(uint32_t src, uint32_t dst, uint32_t *po, size_t pol,
+                uint32_t nbors[D11_MAX_LNS][D11_MAX_LNS],
+                size_t nbor_cts[D11_MAX_LNS]) {
     // printf("src[%"PRIu32"] -> dst[%"PRIu32"]: ", src, dst);
     size_t start = 0;
     while (start < pol && po[start] != dst) {
         start++;
     }
-    // printf("[%zu] [%zu] %"PRIu32" == %"PRIu32"\n", start, pol, src, po[start]);
+    // printf("[%zu] [%zu] %"PRIu32" == %"PRIu32"\n", start, pol, src,
+    // po[start]);
     assert(start < pol && "Did not find dst in the post order!");
     size_t end = 0;
     while (end < pol && po[end] != src) {
@@ -205,7 +213,7 @@ long long paths(uint32_t src, uint32_t dst, uint32_t *po, size_t pol, uint32_t n
 
     int visited[D11_MAX_LNS] = {0};
     visited[dst] = 1;
-    for (size_t idx = start+1; idx <= end; ++idx) {
+    for (size_t idx = start + 1; idx <= end; ++idx) {
         uint32_t curr = po[idx];
         int count = 0;
         uint32_t *curr_nbors = nbors[curr];
@@ -221,8 +229,7 @@ long long paths(uint32_t src, uint32_t dst, uint32_t *po, size_t pol, uint32_t n
     return visited[src];
 }
 
-long long d11_solve_p2(char *input, const size_t input_sz)
-{
+long long d11_solve_p2(char *input, const size_t input_sz) {
     StringView lns = {0};
     size_t lns_ct = 0;
     Splitter split = {.buf = input, .mx = input_sz};
@@ -236,21 +243,24 @@ long long d11_solve_p2(char *input, const size_t input_sz)
     while (lines(&split)) {
         assert("lns ran out of space!" && lns_ct < D11_MAX_LNS);
         split_curr(&split, &lns);
-        Splitter ln_split = {.buf=lns.buf, .mx=lns.len};
+        Splitter ln_split = {.buf = lns.buf, .mx = lns.len};
         delim(&ln_split, ' ');
         StringView node_str;
         split_curr(&ln_split, &node_str);
         --node_str.len;
 
         uint32_t node_idx = get_index(&node_str, &node_strs[0], &populated[0]);
-        // printf("["); sv_print(&node_str); printf("](%"PRIu32")--> ", node_hash);
+        // printf("["); sv_print(&node_str); printf("](%"PRIu32")--> ",
+        // node_hash);
         while (delim(&ln_split, ' ')) {
             StringView nbor_str;
             split_curr(&ln_split, &nbor_str);
-            if (nbor_str.buf[nbor_str.len-1] == '\n') --nbor_str.len;
+            if (nbor_str.buf[nbor_str.len - 1] == '\n') --nbor_str.len;
 
-            uint32_t nbor_idx = get_index(&nbor_str, &node_strs[0], &populated[0]);
-            // printf("["); sv_print(&nbor_str); printf("](%"PRIu32") ", nbor_hash);
+            uint32_t nbor_idx =
+                get_index(&nbor_str, &node_strs[0], &populated[0]);
+            // printf("["); sv_print(&nbor_str); printf("](%"PRIu32") ",
+            // nbor_hash);
             nbors[node_idx][nbor_cts[node_idx]] = nbor_idx;
             nbor_cts[node_idx] += 1;
         }
@@ -271,7 +281,7 @@ long long d11_solve_p2(char *input, const size_t input_sz)
 
     // push svr onto the stack
     // while stack is not empty, pop off stack
-    // 
+    //
     typedef struct {
         uint32_t d;
         int seen;
@@ -283,7 +293,7 @@ long long d11_solve_p2(char *input, const size_t input_sz)
     sf stack[D11_MAX_LNS] = {0};
     size_t stack_size = 0;
 
-    stack[stack_size++] = (sf){ .d = svr, .seen = 0 };
+    stack[stack_size++] = (sf){.d = svr, .seen = 0};
 
     while (stack_size != 0) {
         sf curr = stack[--stack_size];
@@ -295,25 +305,26 @@ long long d11_solve_p2(char *input, const size_t input_sz)
             continue;
         }
         visited[curr.d] = 1;
-        stack[stack_size++] = (sf){ .d = curr.d, .seen = 1 };
+        stack[stack_size++] = (sf){.d = curr.d, .seen = 1};
 
         uint32_t *curr_nbors = nbors[curr.d];
         size_t curr_nbors_ct = nbor_cts[curr.d];
-        // sv_print(&node_strs[curr.d]); printf(" -> %zu nbors\n", curr_nbors_ct);
+        // sv_print(&node_strs[curr.d]); printf(" -> %zu nbors\n",
+        // curr_nbors_ct);
         for (size_t i = 0; i < curr_nbors_ct; ++i) {
             if (!visited[curr_nbors[i]]) {
-                stack[stack_size++] = (sf){ .d = curr_nbors[i], .seen = 0 };
+                stack[stack_size++] = (sf){.d = curr_nbors[i], .seen = 0};
             }
         }
     }
 
     // for (size_t i = 0; i < pol; ++i) {
-    //     sv_print(&node_strs[post_order[i]]); printf(" (%"PRIu32")\n", post_order[i]);
+    //     sv_print(&node_strs[post_order[i]]); printf(" (%"PRIu32")\n",
+    //     post_order[i]);
     // }
 
-
     // svr -> dac -> fft -> out
-    // 
+    //
     // svr -> fft -> dac -> out
     long long svr_fft = paths(svr, fft, post_order, pol, nbors, nbor_cts);
     long long fft_dac = paths(fft, dac, post_order, pol, nbors, nbor_cts);
@@ -326,13 +337,13 @@ long long d11_solve_p2(char *input, const size_t input_sz)
     // printf("%lld * %lld * %lld\n", svr_fft, fft_dac, dac_out);
     // printf("%lld * %lld * %lld\n", svr_dac, dac_fft, fft_out);
 
-    long long total = (svr_fft * fft_dac * dac_out) + (svr_dac * dac_fft * fft_out);
+    long long total =
+        (svr_fft * fft_dac * dac_out) + (svr_dac * dac_fft * fft_out);
 
     return total;
 }
 
-void solve_day11(const char *in_file)
-{
+void solve_day11(const char *in_file) {
     char input[BUF_CAP];
     size_t input_sz;
     if (aoc_slurp_file(in_file, input, &input_sz) != 0) {
@@ -346,7 +357,7 @@ void solve_day11(const char *in_file)
 #include "lib/test.h"
 
 TEST(test_day11_part1) {
-    char *input_str = 
+    char *input_str =
         "aaa: you hhh\n"
         "you: bbb ccc\n"
         "bbb: ddd eee\n"
@@ -360,14 +371,14 @@ TEST(test_day11_part1) {
 
     char input[512];
     size_t input_len = 0;
-    aoc_fill_input(input_str, (char*)&input, &input_len);
+    aoc_fill_input(input_str, (char *)&input, &input_len);
     long long result = d11_solve_p1(input, input_len);
     // printf("%lld\n", result);
     ASSERT(result == 5)
 }
 
 TEST(test_day11_part2) {
-    char *input_str = 
+    char *input_str =
         "svr: aaa bbb\n"
         "aaa: fft\n"
         "fft: ccc\n"
@@ -384,14 +395,13 @@ TEST(test_day11_part2) {
 
     char input[512];
     size_t input_len = 0;
-    aoc_fill_input(input_str, (char*)&input, &input_len);
+    aoc_fill_input(input_str, (char *)&input, &input_len);
     long long result = d11_solve_p2(input, input_len);
     // printf("%lld\n", result);
     ASSERT(result == 2)
 }
 
-void day11_tests()
-{
+void day11_tests() {
     RUN_TEST(test_day11_part1);
     RUN_TEST(test_day11_part2);
 }
